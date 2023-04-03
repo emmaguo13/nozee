af4281e9e870ed6ef7c482ee94fc6bff9b5db01f
Sehyun Chung <sehyun@berkeley.edu>
2023-04-03 15:28:29 -0700

    prettier

--- pages/api/write.ts
+++ pages/api/write.ts
@@ -1,58 +1,70 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { initializeApp, applicationDefault, getApp, App, getApps, cert, Credential, ServiceAccount } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import { NextApiRequest, NextApiResponse } from 'next'
+import {
+  initializeApp,
+  applicationDefault,
+  getApp,
+  App,
+  getApps,
+  cert,
+  Credential,
+  ServiceAccount
+} from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
 import { Post } from '../../types'
 
 import { vkey } from './constants/vkey'
 const snarkjs = require('snarkjs')
 
-let app: App;
+let app: App
 
 let serviceAcc = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
+  privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n')
 }
 
 if (getApps().length == 0) {
-    app = initializeApp({
-        credential: cert(serviceAcc as ServiceAccount),
-        projectId: process.env.NEXT_PUBLIC_PROJECT_ID
-    }, "nozee");
+  app = initializeApp(
+    {
+      credential: cert(serviceAcc as ServiceAccount),
+      projectId: process.env.NEXT_PUBLIC_PROJECT_ID
+    },
+    'nozee'
+  )
 } else {
-    app = getApp("nozee")
+  app = getApp('nozee')
 }
 
 const db = getFirestore(app)
 
 async function createPost({
-    id,
-    company,
-    message,
-    address,
-    signature,
-    title
-  }: Post) {
-    return db
-      .collection('posts')
-      .doc(id)
-      .set({
-        title,
-        company,
-        message,
-        address,
-        signature,
-        id,
-        timestamp: Date.now()
-      })
-      .then(docRef => {
-        console.log('Document written with ID: ', docRef)
-        return docRef
-      })
-      .catch(error => {
-        throw new Error(error);
-      })
-  }
+  id,
+  company,
+  message,
+  address,
+  signature,
+  title
+}: Post) {
+  return db
+    .collection('posts')
+    .doc(id)
+    .set({
+      title,
+      company,
+      message,
+      address,
+      signature,
+      id,
+      timestamp: Date.now()
+    })
+    .then(docRef => {
+      console.log('Document written with ID: ', docRef)
+      return docRef
+    })
+    .catch(error => {
+      throw new Error(error)
+    })
+}
 
 export async function verifyProof(proof: any, publicSignals: any) {
   const proofVerified = await snarkjs.groth16.verify(vkey, publicSignals, proof)
@@ -62,9 +74,8 @@ export async function verifyProof(proof: any, publicSignals: any) {
 // this allows you to write to a post
 export default async function handler(
   request: NextApiRequest,
-  response: NextApiResponse,
+  response: NextApiResponse
 ) {
-
   const { body } = request
   const b = JSON.parse(body)
 
@@ -77,20 +88,20 @@ export default async function handler(
     title
   */
 
-  // verify proof here 
+  // verify proof here
   const isVerified = await verifyProof(b.proof, b.publicInputs)
 
   if (isVerified) {
-    let post; 
+    let post
     try {
       post = await createPost(b)
       // post to web3.storage
     } catch {
-      return response.status(500).send("Database write error")
+      return response.status(500).send('Database write error')
     }
   } else {
-    return response.status(400).send("Proof not verified")
+    return response.status(400).send('Proof not verified')
   }
 
-  return response.status(200).send("Success")
+  return response.status(200).send('Success')
 }
