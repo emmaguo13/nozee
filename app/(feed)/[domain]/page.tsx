655673d3ae56e0c7d1ee6258d01aebeae8eb82cf
Sehyun Chung <sehyun@berkeley.edu>
2023-06-19 19:04:17 -0400

    migrate firebase filtered posts

--- app/(feed)/[domain]/page.tsx
+++ app/(feed)/[domain]/page.tsx
@@ -1,22 +1,31 @@
 import { Post } from "@/types"
+import * as admin from "firebase-admin"
 
 import { PostCard } from "@/components/post-card"
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  })
+}
+
+const db = admin.firestore()
 
 export default async function Page({
   params: { domain },
 }: {
   params: { domain: string }
 }) {
-  const posts = await fetch(
-    process.env.NEXT_PUBLIC_BASE_URL + "/api/filtered-posts",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ domain }),
-    }
-  ).then((res) => res.json())
+  const snapshot = await db
+    .collection("posts")
+    .where("domain", "==", domain)
+    .orderBy("timestamp", "desc")
+    .get()
+  const posts = snapshot.docs.map((doc) => doc.data() as Post)
   return (
     <>
       {posts.map((post: Post) => (
